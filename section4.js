cd356c20acacf8b1b158e2f77de434416da4c9cf
takumii46 <garikkrass@gmail.com>
2026-03-13 19:58:46 +0100

    Object and how to avoid their mutations

--- section4.js
+++ section4.js
@@ -1,1 +1,97 @@
-section.js
+'use strict'
+
+const myBackpack = {
+  liquid: 'water',
+  food: 'can of beans',
+  gadget: false,
+}
+
+const hisBackpack = {
+  food: 'can of beans',
+  liquid: 'water',
+  gadget: false,
+}
+
+hisBackpack.weapon = 'pistol'
+myBackpack.food = 'half eaten can of beans'
+
+// console.log(myBackpack.gadget)
+// console.log(hisBackpack)
+// console.log(myBackpack)
+
+//TODO delete operator
+delete hisBackpack.liquid
+// console.log(hisBackpack)
+
+//TODO adding a property with []
+myBackpack['explosive'] = 'hand grenade'
+delete myBackpack['liquid']
+
+console.log(myBackpack)
+
+//TODO best example when bracket notation [] is used
+const backpackPropertyName = 'explosive'
+hisBackpack[backpackPropertyName] = 'flashbang'
+
+console.log(hisBackpack)
+
+//TODO reaching object hierarchy
+const tools = {
+  hammer: 17,
+  axe: 4,
+  sword: 11,
+  proTools: {
+    catapult: 2,
+    cannon: 6,
+  },
+}
+
+tools.proTools['bomb'] = 100
+
+console.log(tools)
+
+console.log(tools.proTools.cannon)
+
+//TODO variable usage
+const name = 'bogdan'
+const postsQty = 35
+
+const userProfile = {
+    name: name, // we can shorten it to just 'name,'
+    postsQty: postsQty, //here as well 'postsQty,'
+    isHired: false
+}
+
+//TODO mTODO ethods
+const myCity = {
+    city: 'New York',
+    cityGreeting: function () {
+         console.log('Greetings!')
+     }
+}
+
+console.log(myCity)
+myCity.cityGreeting()
+
+//we can shorten function in the object
+
+const myCountry = {
+    country: 'Ukraine',
+    countryGreeting() { 
+        console.log('Здарова')
+    }
+}
+
+myCountry.countryGreeting()
+
+//TODO converting JSON to JavaScript and Js to JSON
+const myStats = {
+    age: 16,
+    name: 'igor',
+    hobby: 'CODING'
+}
+
+JSON.stringify(myStats) //myStats is now a string in JSON
+
+const myStatsStringified = JSON.stringify(myStats) //we can use this variable to parse myStats
+JSON.parse(myStatsStringified) //myStats is js again
---
+++ section5.js
@@ -0,0 +1,81 @@
+//mutations
+//TODO copy by value
+const a = 10
+let b = a
+
+b = 30
+
+console.log(a)
+console.log(b)
+
+//values of reference type
+
+const person = {
+  name: 'Dima',
+  age: 17,
+}
+
+person.age = 22
+person.isAdult = false
+console.log(person.age)
+console.log(person.isAdult)
+
+//we changed value, followingly we mutated the object despite that the object was declared as const
+
+//TODO mutation of (object's) copies
+
+const coWorker = {
+  name: 'bogdan',
+  stack: 'frontend',
+  yearsOfXp: 10,
+}
+
+const coWorkerCopy = coWorker //we copied the reference of coWorker and mutated coWorker
+
+coWorker.name = 'igor'
+coWorker.yearsOfXp = 0
+
+console.log(coWorkerCopy)
+console.log(coWorker)
+
+//TODO how to avoid mutations
+// variant1: Object.assign({}, variable)
+const guy = {
+  name: 'yuri',
+  age: 33,
+}
+
+const guy2 = Object.assign({}, guy)
+guy2.age = 34
+
+console.log(guy)
+console.log(guy2)
+
+//variant2: {...variable} ... is spread
+const boy = {
+  name: 'mikhail',
+  age: 12,
+}
+
+const boy2 = { ...boy }
+boy2.age = 13
+
+console.log(boy)
+console.log(boy2)
+
+//variant3 JSON.parse(JSON.stringify(variable)), complete avoidance of mutation
+const swagBoy = {
+  name: 'darren',
+  age: 22,
+  coolStuff: {
+    weed: '10 grams',
+    alcohol: '10 cans of beer',
+  },
+}
+
+const swagBoy2 = JSON.parse(JSON.stringify(swagBoy))
+swagBoy2.age = 23
+swagBoy2.coolStuff.alcohol = '8 cans of beer'
+
+console.log(swagBoy)
+console.log(swagBoy2)
